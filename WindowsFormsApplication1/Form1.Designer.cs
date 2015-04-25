5305513a209cb2a06fc9e50db46d2e4afda4be69
george070393 <george070393@gmail.com>
2015-04-25 18:07:01 +0300

    add humanizer

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -47,10 +47,11 @@
             // 
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "Romana",
-            "Engleza",
-            "Germana",
-            "Franceza"});
+            "Romanian",
+            "English",
+            "German",
+            "French",
+            "Spanish"});
             this.comboBox1.Location = new System.Drawing.Point(114, 16);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -4,42 +4,74 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Humanizer;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        
+       
         public Form1()
         {
             InitializeComponent();
-        }
-
-       
-
-        public void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            TextBox objTextBox = (TextBox)sender;
-            string theText = objTextBox.Text;
             
         }
 
-        public void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        
+
+        public string textBox1_TextChanged(object sender, EventArgs e)
         {
-            comboBox1.Items.Add(new object[1] { "Romana" });
+            
+            TextBox objTextBox = (TextBox)sender;
+            string insertText = objTextBox.Text;
+            return insertText;
+        }
+
+        
+
+        public string comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           string selected = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
+            return selected;
+
         }
 
         public void button1_Click(object sender, EventArgs e)
         {
-            
-            MessageBox.Show( theText , "My Application",
-            MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+
+            string insertText;
+            string selected;
+            string result;
+            int number;
+            number = Convert.ToInt32(insertText);
+            if (selected == "English")
+            {
+                result = number.ToWords(culture: English);
+            }
+             if (selected == "German")
+             {
+                 result = number.ToWords(culture: German);
+             }
+             if (selected == "Spanish")
+             {
+                 result = number.ToWords(culture: Spanish);
+             }
+             if (selected == "French")
+             {
+                 result = number.ToWords(culture: French);
+             }
+             else
+             {
+                 result = number.ToWords(culture: Romanian);
+             }
+            MessageBox.Show( result , "The number is:",
+MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
         }
-
-       
-
-        
     }
 }
+
